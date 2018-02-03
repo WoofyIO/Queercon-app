b96011a9536136d789d48222b5bc9c7fbc11617f
Jake Visser <jake@queercon.org>
2018-02-02 23:35:42 -0800

    codepush
    enabling on resume sync

--- Queercon/src/app/app.component.ts
+++ Queercon/src/app/app.component.ts
@@ -17,6 +17,8 @@ export class MyApp {
       // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
       Splashscreen.hide();
+	  platform.resume.subscribe(() => 
+		codePush.sync();
     });
   }
 }
