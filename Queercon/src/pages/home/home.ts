9608f7b764372c2743fd36b589e7a108f628ead1
Jonathan Nelson <eurotwelve@gmail.com>
2018-03-25 22:23:22 -0700

    Very first page navigation
    Placeholder for event nav.

--- Queercon/src/pages/home/home.ts
+++ Queercon/src/pages/home/home.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { EventsPage } from '../events/events';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
 export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
   }
 
+	goToEventsPage(){
+		this.navCtrl.setRoot(EventsPage);
+	}
 }
