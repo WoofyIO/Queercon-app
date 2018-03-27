af61dc0a505d2684d76c3dd128ca15df3ee1f855
Jonathan Nelson <eurotwelve@gmail.com>
2018-03-26 21:26:29 -0700

    Wiring up page headers
    All pages have headers, and links from home and menu button should work now.

--- Queercon/src/pages/home/home.ts
+++ Queercon/src/pages/home/home.ts
@@ -2,7 +2,15 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 import { Qc15Page } from '../qc15/qc15';
+import { ChatPage } from '../chat/chat';
+import { LocalPage } from '../local/local';
 import { QcRsa2018Page } from '../qc-rsa2018/qc-rsa2018';
+import { VolunteerPage } from '../volunteer/volunteer';
+import { EventsPage } from '../events/events';
+import { ContactPage } from '../contact/contact';
+import { SupportersPage } from '../supporters/supporters';
+import { AboutPage } from '../about/about';
+import { NewsPage } from '../news/news';
 
 @Component({
   selector: 'page-home',
@@ -15,12 +23,15 @@ export class HomePage {
 
   }
 
-	goToQc15Page(){
-		this.navCtrl.setRoot(Qc15Page);
-	}
-
-	goToQcRsaPage(){
-		this.navCtrl.setRoot(QcRsa2018Page);
-	}
+	goToNewsPage(){ this.navCtrl.setRoot(NewsPage); }
+	goToLocalPage(){ this.navCtrl.setRoot(LocalPage); }
+	goToAboutPage(){ this.navCtrl.setRoot(AboutPage); }
+	goToVolunteerPage(){ this.navCtrl.setRoot(VolunteerPage); }
+	goToContactPage(){ this.navCtrl.setRoot(ContactPage); }
+	goToSupportersPage(){ this.navCtrl.setRoot(SupportersPage); }
+	goToChatPage(){ this.navCtrl.setRoot(ChatPage); }
+	goToEventsPage(){ this.navCtrl.setRoot(EventsPage); }
+	goToQc15Page(){ this.navCtrl.setRoot(Qc15Page); }
+	goToQcRsaPage(){ this.navCtrl.setRoot(QcRsa2018Page); }
 
 }
