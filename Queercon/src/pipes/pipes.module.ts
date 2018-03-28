45ac0caf7f1dd4dbe686441420abea49d530c047
Jake Vis <visser.jacob@outlook.com>
2018-03-28 10:08:15 -0700

    removing pipe
    not needed

--- Queercon/src/pipes/pipes.module.ts
+++
@@ -1,8 +0,0 @@
-import { NgModule } from '@angular/core';
-import { EventListPipe } from './event-list/event-list';
-@NgModule({
-	declarations: [EventListPipe],
-	imports: [],
-	exports: [EventListPipe]
-})
-export class PipesModule {}
