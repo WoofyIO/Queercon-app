da507900c1e6666b4ca701df11be3362d806631b
Jonathan Nelson <eurotwelve@gmail.com>
2018-03-29 22:06:10 -0700

    Circular supporters page

--- Queercon/src/pages/supporters/supporters.ts
+++ Queercon/src/pages/supporters/supporters.ts
@@ -8,7 +8,6 @@ import { QcRsa2018Page } from '../qc-rsa2018/qc-rsa2018';
 import { VolunteerPage } from '../volunteer/volunteer';
 import { EventsPage } from '../events/events';
 import { ContactPage } from '../contact/contact';
-import { SupportersPage } from '../supporters/supporters';
 import { AboutPage } from '../about/about';
 import { NewsPage } from '../news/news';
 
@@ -30,7 +29,6 @@ export class SupportersPage {
 	goToAboutPage(){ this.navCtrl.setRoot(AboutPage); }
 	goToVolunteerPage(){ this.navCtrl.setRoot(VolunteerPage); }
 	goToContactPage(){ this.navCtrl.setRoot(ContactPage); }
-	goToSupportersPage(){ this.navCtrl.setRoot(SupportersPage); }
 	goToChatPage(){ this.navCtrl.setRoot(ChatPage); }
 	goToEventsPage(){ this.navCtrl.setRoot(EventsPage); }
 	goToQc15Page(){ this.navCtrl.setRoot(Qc15Page); }
