cb9b1388b7153a1f172ea9873808068f316191b2
Jake Vis <visser.jacob@outlook.com>
2018-04-02 20:26:04 -0700

    #41

--- Queercon/src/pages/events/events.ts
+++ Queercon/src/pages/events/events.ts
@@ -25,10 +25,12 @@ export class EventsPage {
   /* Remove in staging *///private webWiew: any = window;
   items = [];
   constructor(public nav: NavController, http: Http) {
+    console.log('QC Events constructor loaded');
     /* Remove in staging *///this.webWiew.AppCenter.Analytics.trackEvent('EventsPage Loaded');
     let localData = http.get('assets/js/events.json').map(res => res.json().events);
     localData.subscribe(data => {
       this.items = data;
+      console.log('QC LocalData');
     })
   }
 
