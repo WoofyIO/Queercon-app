a2abb42af6b26a7c25e97c7819a34b0a58bd8903
Jake Vis <visser.jacob@outlook.com>
2018-04-03 20:41:28 -0700

    Fixing Statusbar
    Working on #45

--- Queercon/src/app/app.component.ts
+++ Queercon/src/app/app.component.ts
@@ -60,7 +60,8 @@ export class MyApp {
 
 		this.platform.ready().then(() => {
 			this.statusBar.styleDefault();
-			this.statusBar.overlaysWebView(false); //adding padding for iOS
+			this.statusBar.overlaysWebView(true); //adding padding for iOS
+			this.statusBar.backgroundColorByHexString('#ffffff');
 			this.splashScreen.hide();
 
 
