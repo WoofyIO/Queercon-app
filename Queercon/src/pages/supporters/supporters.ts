74f8fb2edb0bcb311cdb33606c27f32e9802a2fa
Jake Vis <visser.jacob@outlook.com>
2018-04-08 00:34:18 -0700

    updates to events
    Working on #49  and #48

--- Queercon/src/pages/supporters/supporters.ts
+++ Queercon/src/pages/supporters/supporters.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, Platform } from 'ionic-angular';
 
 import { Qc15Page } from '../qc15/qc15';
 import { ChatPage } from '../chat/chat';
@@ -17,10 +17,13 @@ import { NewsPage } from '../news/news';
 })
 export class SupportersPage {
 
-  /* Remove in staging *///private webWiew: any = window;
+  private webWiew: any = window;
 
-  constructor(public navCtrl: NavController) {
-    /* Remove in staging *///this.webWiew.AppCenter.Analytics.trackEvent('Supporters Loaded');
+  constructor(public platform: Platform, public navCtrl: NavController) {
+		if (!this.platform.is('mobileweb')) {
+      console.log("queercon aaccaa loading not mobileweb");
+      this.webWiew.AppCenter.Analytics.trackEvent('QC supporters.ts');		
+    } 
 
   }
 
